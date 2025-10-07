c73d45cf524cbc9649a19aa90a598dd7383531bc
Anushka Gaat <anushkagaat@gmail.com>
2025-10-07 00:16:53 -0500

    added homepage and browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-// src/App.tsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
-import DetailPage from "./pages/DetailPage";
-import "./App.css";
 import GalleryPage from "./pages/GalleryPage";
+import DetailPage from "./pages/DetailPage";
+import Navbar from "./components/Navbar";
 
-const App: React.FC = () => (
-  <Router>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/gallery">Gallery</Link>
-    </nav>
-    <Routes>
-      <Route path="/" element={<SearchPage />} />
-      <Route path="/gallery" element={<GalleryPage />} />
-      <Route path="/artwork/:id" element={<DetailPage />} />
-    </Routes>
-  </Router>
-);
+function App() {
+  return (
+    <BrowserRouter basename="/mp2">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+        <Route path="/artwork/:id" element={<DetailPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
